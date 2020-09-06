91be38a644dcf0b1c582a1ccfeede088c470b1f1
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-06 12:50:48 +0500

    About us component completed

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { default as Header } from './Header/Header';
 export { default as Hero } from './Hero/Hero';
 export { default as FeaturedProducts } from './FeaturedProducts/FeaturedProducts';
 export { default as OurProducts } from './OurProducts/OurProducts';
+export { default as AboutUs } from './AboutUs/AboutUs';
 export { default as Footer } from './Footer/Footer';
