01ae95a3da8c981392fa6831f3fcd7cf96f3cbc0
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-06 21:19:02 +0500

    completed Contact Us component - used Grid, TextField and Button

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export { default as Hero } from './Hero/Hero';
 export { default as FeaturedProducts } from './FeaturedProducts/FeaturedProducts';
 export { default as OurProducts } from './OurProducts/OurProducts';
 export { default as AboutUs } from './AboutUs/AboutUs';
+export { default as ContactUs } from './ContactUs/ContactUs';
 export { default as Footer } from './Footer/Footer';
