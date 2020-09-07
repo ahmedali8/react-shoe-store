f4382eab15d2a4af639adf569412aba2d3b13f2f
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-07 22:58:59 +0500

    added a Home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Hero, FeaturedProducts, OurProducts } from '../../components';
+
+const Home = () => {
+    return (
+        <div>
+            <Hero />
+            <FeaturedProducts />
+            <OurProducts />
+        </div>
+    );
+};
+
+export default Home;
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 export { default as Header } from './Header/Header';
+export { default as Home } from './Home/Home';
 export { default as Hero } from './Hero/Hero';
 export { default as FeaturedProducts } from './FeaturedProducts/FeaturedProducts';
 export { default as OurProducts } from './OurProducts/OurProducts';
 export { default as ProductIndex } from './ProductIndex/ProductIndex';
+export { default as ProductDetails } from './ProductDetails/ProductDetails';
 export { default as AboutUs } from './AboutUs/AboutUs';
 export { default as ContactUs } from './ContactUs/ContactUs';
 export { default as Footer } from './Footer/Footer';
