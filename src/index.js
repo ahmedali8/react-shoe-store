fb290b1ef1be3b9cf97c922bbf8817f97e901dfa
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-07 22:59:16 +0500

    added Router tag

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalProvider } from './context/GlobalState';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -9,12 +10,14 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 
 ReactDOM.render(
-    <GlobalProvider>
-        <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <App />
-        </ThemeProvider>
-    </GlobalProvider>,
-	document.getElementById('root')
+    <Router>
+        <GlobalProvider>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
+        </GlobalProvider>
+    </Router>,
+    document.getElementById('root')
 );
