dcb6c3fa15172cab862175393209de83a34c91d9
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-07 18:48:40 +0500

    added assets/images folder in public folder

--- src/images/index.js
+++
@@ -1,6 +0,0 @@
-export { default as shoe1Png } from './shoe-1.png';
-export { default as shoe2Png } from './shoe-2.png';
-export { default as shoe3Png } from './shoe-3.png';
-export { default as shoe4Png } from './shoe-4.png';
-export { default as shoe5Png } from './shoe-5.png';
-export { default as shoe6Png } from './shoe-6.png';
