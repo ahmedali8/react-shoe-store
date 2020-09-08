bd4039f4d0536c6281bc039a590cbde15845538b
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-08 19:45:36 +0500

    added Routes to pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
-import { Header, Hero, FeaturedProducts, OurProducts, Footer } from './components';
+import { Header, Home, FeaturedProducts, NewArrivals, Footer, AboutUs, ContactUs, ProductIndex, ProductDetails, NotFound } from './components';
+import { Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/Header/ScrollToTop/ScrollToTop';
 
 import './App.module.css';
-import ScrollToTop from './components/Header/ScrollToTop/ScrollToTop';
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Hero />
-			<FeaturedProducts />
-			<OurProducts />
-			<ScrollToTop />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="featured-products" element={<FeaturedProducts />} />
+                <Route path="new-arrivals" element={<NewArrivals />} />
+
+                <Route path="product-index" element={<ProductIndex />} />
+                <Route path="product-index/:productId" element={<ProductDetails />} />
+                
+                <Route path="about-us" element={<AboutUs />} />
+                <Route path="contact-us" element={<ContactUs />} />
+
+                {/* if nothing matches above then: */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+
+			<ScrollToTop /> 
 			<Footer />
 		</div>
 	);
