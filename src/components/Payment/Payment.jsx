3ac2fea7e5d4fe47cb8ce2469728de022e1eb6ef
ahmedali8 <imahmedalibhatti@gmail.com>
2020-09-10 10:32:13 +0500

    Payment component complete

---
+++ src/components/Payment/Payment.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Payment = () => {
+    return (
+        <div style={{textAlign: 'center', height: '100vh'}}>
+            <h1 >Payment Component</h1>
+            <p>Credit card features could be added here</p>
+        </div>
+    );
+};
+
+export default Payment;
